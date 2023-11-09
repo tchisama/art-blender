f0c8a1c97d8feb2ea5d52bd363c4bab2c039728e
tchi-sama <tchisama41@gmail.com>
2023-11-09 13:29:51 -0800

    add icons

--- app/playground/components/WorkSpace.tsx
+++ app/playground/components/WorkSpace.tsx
@@ -1,22 +1,32 @@
-import { Button, Textarea } from '@nextui-org/react'
-import React from 'react'
-
-type Props = {}
+import { Button, Textarea } from "@nextui-org/react";
+import React from "react";
+import { RiMagicFill } from "react-icons/ri";
+type Props = {};
 
 function WorkSpace({}: Props) {
   return (
-    <div className='p-4 flex-1'>
-        <h2 className='text-xl'>AI Image Generation</h2>
-        <div className='flex mt-4 gap-2'>
+    <div className="p-4 flex-1">
+      <h2 className="text-xl my-4">AI Image Generation</h2>
+      <div className="flex mt-4 gap-2">
+        <Button size="lg" variant="bordered" isIconOnly>
+          <RiMagicFill />
+        </Button>
         <Textarea
-            label="Prompt"
-            placeholder="type your prompt .."
-            className="flex-1"
+          label="Prompt"
+          placeholder="type your prompt .."
+          className="flex-1"
         />
-        <Button size='lg' color='primary' className=''>Generate</Button>
-        </div>
+        <Button
+          size="lg"
+          color="secondary"
+          className=""
+          startContent={<RiMagicFill />}
+        >
+          Generate
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default WorkSpace
+export default WorkSpace;
